dd3d99a91ffa219059051e96575e2d62c7a5d836
YordyM <yordi.romero22@itca.edu.sv>
2023-11-15 20:49:07 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/yirm/proyect1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yirm.proyect1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Webview Example'),
+        ),
+        body: WebViewExample(),
+      ),
+    );
+  }
+}
+
+class WebViewExample extends StatefulWidget {
+  @override
+  WebViewExampleState createState() => WebViewExampleState();
+}
+
+class WebViewExampleState extends State<WebViewExample> {
+  final String url =
+      "https://mastermky.com/carrito/"; // Reemplaza esto con la URL que deseas mostrar.
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: url,
+      javascriptMode: JavascriptMode.unrestricted,
+    );
+  }
+}
